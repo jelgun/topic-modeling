ea709a149ff2b1a17d7aca97e251c3f6e16d3b48
Elgun <elgun1999@gmail.com>
2019-09-19 23:39:16 +0400

    upd

--- lda.py
+++ lda.py
@@ -13,9 +13,6 @@
 """
 import json
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer
@@ -23,6 +20,10 @@ from string import punctuation
 from gensim.corpora import Dictionary
 from gensim.models import LdaMulticore
 from gensim.models.coherencemodel import CoherenceModel
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 
 def parse(files):
     documents = []
@@ -33,28 +34,33 @@ def parse(files):
                 documents.append(i['text'])
     return documents
 
+
 def preprocess():
-    files = ['wikisection_dataset_json/wikisection_en_city_train.json', 'wikisection_dataset_json/wikisection_en_disease_train.json']
+    files = ['wikisection_dataset_json/wikisection_en_city_train.json',
+             'wikisection_dataset_json/wikisection_en_disease_train.json']
     documents = parse(files)
 
-    #tokenize
+    # tokenize
     tokenized_data = []
     for data in documents:
         tokenized_data.append(i.lower() for i in word_tokenize(data))
 
-    #remove stopwords, numbers and punctuations
-    stop_words = set(stopwords.words('english')) 
+    # remove stopwords, numbers and punctuations
+    stop_words = set(stopwords.words('english'))
     filtered_data = []
     for data in tokenized_data:
-        filtered_data.append([w for w in data if (w not in stop_words) and (not w.isdigit()) and (w not in punctuation)])
+        filtered_data.append([w for w in data
+                             if (w not in stop_words) and
+                             (not w.isdigit()) and
+                             (w not in punctuation)])
 
-    #lemmatize
+    # lemmatize
     lemmatizer = WordNetLemmatizer()
     lemm_data = []
     for data in filtered_data:
         lemm_data.append([lemmatizer.lemmatize(w) for w in data])
-    
-    #stem
+
+    # stem
     ps = PorterStemmer()
     stem_data = []
     for data in lemm_data:
@@ -62,21 +68,34 @@ def preprocess():
 
     return stem_data
 
+
 def create_bow(data):
     dct = Dictionary(data)
     dct.filter_extremes(no_below=20)
     bow = [dct.doc2bow(doc) for doc in data]
     return dct, bow
 
+
 data = preprocess()
 dct, bow = create_bow(data)
-lda_model = LdaMulticore(bow, num_topics=2, id2word=dct, passes=5, workers=2)
+lda_model = LdaMulticore(
+    corpus=bow,
+    num_topics=2,
+    id2word=dct,
+    passes=5,
+    workers=2)
 
-#word weights for topics
+# word weights for topics
 for idx, topic in lda_model.print_topics(-1):
     print('Topic: {} \nWords: {}'.format(idx, topic))
 
-#Topic Coherence
-coherence_model_lda = CoherenceModel(model=lda_model, texts=data, corpus=bow, dictionary=dct, coherence='c_v')
+# Topic Coherence
+coherence_model_lda = CoherenceModel(
+    model=lda_model,
+    texts=data,
+    corpus=bow,
+    dictionary=dct,
+    coherence='c_v')
+
 coherence = coherence_model_lda.get_coherence()
 print('\nCoherence Score: ', coherence)

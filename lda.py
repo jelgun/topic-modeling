c816d4a1c5ae8b3dba5aa31372a7688e9259e56a
Elgun Jabrayilzade <elgun1999@gmail.com>
2019-10-27 12:41:03 +0300

    Update lda.py

--- lda.py
+++ lda.py
@@ -34,17 +34,72 @@ disease_train_data, disease_train_labels, disease_train_sizes = parse_json('en_d
 disease_test_data, disease_test_labels, disease_test_sizes = parse_json('en_disease_test.json')
 
 # doc_size - frequency graph
-plt.hist(city_train_sizes, bins=200, range=(0, 300))
-plt.show()
-"""
-doc_freq = {}
-for i in city_train_sizes:
-    if i not in doc_freq:
-        doc_freq[i] = 0
-    else:
-        doc_freq[i] += 1
 
-"""
+# plt.hist(city_train_sizes, bins=50, range=(0, 400))
+# plt.hist(disease_train_sizes, bins=50, range=(0,400))
+
+city_sections = [
+    (0, 0, 8),
+    (1, 9, 15),
+    (2, 16, 31),
+    (3, 32, 40),
+    (4, 41, 47),
+    (5, 48, 63),
+    (6, 64, 103),
+    (7, 104, 111),
+    (8, 112, 119),
+    (9, 120, 135),
+    (10, 136, 143),
+    (11, 144, 151),
+    (12, 152, 167),
+    (13, 168, 199),
+    (14, 200, 223),
+    (15, 224, 247),
+    (16, 248, 255),
+    (17, 256, 279),
+    (18, 280, 295),
+    (19, 296, 303),
+    (20, 304, 319),
+    (21, 320, 367),
+    (22, 368, 391),
+    (23, 392, 399),
+    (24, 400, 1000)
+]
+
+disease_sections = [
+    (0, 0, 7),
+    (1, 7, 15),
+    (2, 16, 23),
+    (3, 24, 39),
+    (4, 40, 55),
+    (5, 56, 71),
+    (6, 72, 87),
+    (7, 88, 103),
+    (8, 104, 111),
+    (9, 112, 127),
+    (10, 128, 143),
+    (11, 144, 167),
+    (12, 168, 175),
+    (13, 176, 199),
+    (14, 200, 207),
+    (15, 208, 223),
+    (16, 224, 239),
+    (17, 240, 263),
+    (18, 264, 279),
+    (19, 280, 327),
+    (20, 328, 335),
+    (21, 336, 343),
+    (22, 344, 359),
+    (23, 360, 391),
+    (24, 392, 399),
+    (25, 400, 1000)
+]
+
+for group_no, left_border, right_border in disease_sections:
+    print(group_no, left_border, right_border)
+
+
+
 """
 dct, bow = create_bow(train_data)
 print("Preprocessing is finished!")
